2fb229ff042dd827938e2e75548bac6e77d737a3
Thalia <thaliatelopoulou@gmail.com>
2023-05-12 11:06:08 +0300

    updated some exercises

--- exercise-9/exercise-1.js
+++ exercise-9/exercise-1.js
@@ -4,5 +4,8 @@ const person = {
   age: 25
 }
 
-
+let keysArr = Object.keys(person);
+keysArr.forEach((el => {
+  console.log(el + ": " + person[el]);
+}))
 // Print values of person using Object.keys
