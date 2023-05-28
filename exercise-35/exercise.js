f3468312beeb050739639d70b8ae1f201bfe13d1
Thalia <thaliatelopoulou@gmail.com>
2023-05-28 18:00:39 +0300

    updated till exercise 36

--- exercise-35/exercise.js
+++ exercise-35/exercise.js
@@ -1,1 +1,6 @@
 
+function repeatHello(callback){
+    let repeat = setInterval(callback, 1000);
+};
+const sayHello = () => console.log(`Hello`);
+repeatHello(sayHello);
--- exercise-36/exercise.js
+++ exercise-36/exercise.js
@@ -1,1 +1,6 @@
-
+function repeatHello(callback, n){
+    let repeat = setInterval(callback, 1000);
+    setTimeout(() => clearInterval(repeat), 1000 * n);
+};
+const sayHello = () => console.log(`Hello`);
+repeatHello(sayHello, 5);
